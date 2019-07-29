3164ad6eb9d79f6761a4510ed480ad24fa3ceb0e
Jon Caneiro <joncaneiro@gmail.com>
2019-07-29 07:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  [...kittens, name]
+  return kittens
+}
